a2477bc5d7102068f7a867c47fde7936d46f96d9
Pooja.R <poojaramadhas546@gmail.com>
2026-07-19 17:54:43 +0530

    api.js

--- BookNova/src/services/api.js
+++ BookNova/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://booknova-backend-0nne.onrender.com",
+  baseURL: "https://booknova-backend-0nne.onrender.com/api",
 });
 
 export default api;
