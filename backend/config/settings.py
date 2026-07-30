e676e3fbd5a689568c2f9443b575f57f19bd98b8
PoojaR2804 <poojaramadhas546@gmail.com>
2026-07-30 23:00:33 +0530

    Add SimpleJWT app configuration

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     "rest_framework",
+    "rest_framework_simplejwt",
     "corsheaders",
 
     "books",
@@ -42,7 +43,6 @@ INSTALLED_APPS = [
     "cart",
     "orders",
 ]
-
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
